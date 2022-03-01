316a859d0e0e6e021094fccc43240dbb47c231e2
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-03-01 18:36:28 +0530

    header updated

--- tuition-student-records/client/src/components/Header.jsx
+++ tuition-student-records/client/src/components/Header.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export const Header = () => {
+  const [mobnav, setmobnav] = useState(false);
+  const handleNav = ()=>{
+    setmobnav(!mobnav);
+  }
   return (
     <header>
         <ul>
            <Link to="/"> <li>Home</li></Link> 
            <li id="nav" >
                <Link to="/students"> <li>Students</li></Link> 
-                <Link to="/tests"> <li>Tests</li></Link>
+              <Link to="/tests"> <li>Tests</li></Link>
             </li>
-            
+            <button onClick={handleNav}  className="nav-btn">Menu</button>
         </ul>
+        <div style={mobnav ? {display: 'flex'} : {display:"none"}} className="mobile-menu"> 
+               <Link to="/students">Students</Link> 
+              <Link to="/tests"> Tests</Link>
+   
+          </div>
     </header>
   )
 }
